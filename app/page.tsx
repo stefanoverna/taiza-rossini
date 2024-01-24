767f68aaf2a0c0f373cc844855dcfddaba14dad5
Stefano Verna <s.verna@datocms.com>
2024-01-24 17:00:23 +0100

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import { HorizontalScroller } from '@/components/HorizontalScroller';
-import { Tile } from '@/components/Tile';
-import { UnwrapStructuredText } from '@/components/UnwrapStructuredText';
-import { Work } from '@/components/Work';
-import { HomeDocument, HomeQuery } from '@/graphql/generated';
-import { gql, request } from '@/lib/dato';
-import { renderMetaTags } from 'react-datocms/seo';
+import { HorizontalScroller } from "@/components/HorizontalScroller";
+import { Tile } from "@/components/Tile";
+import { UnwrapStructuredText } from "@/components/UnwrapStructuredText";
+import { Work } from "@/components/Work";
+import { HomeDocument, HomeQuery } from "@/graphql/generated";
+import { gql, request } from "@/lib/dato";
+import { renderMetaTags } from "react-datocms/seo";
 import {
-  StructuredText,
-  StructuredTextDocument,
-} from 'react-datocms/structured-text';
+	StructuredText,
+	StructuredTextDocument,
+} from "react-datocms/structured-text";
 
 const query = gql`
   query Home {
@@ -27,7 +27,7 @@ const query = gql`
       }
     }
 
-    works: allWorks(orderBy: position_ASC) {
+    works: allWorks(orderBy: position_ASC, first: 100) {
       id
       coverImage {
         responsiveImage(imgixParams: { auto: format, h: 1400 }) {
@@ -47,43 +47,43 @@ const query = gql`
 `;
 
 export default async function Home() {
-  const {
-    homepage,
-    works,
-    meta: { count },
-  } = await request<HomeQuery>(query);
+	const {
+		homepage,
+		works,
+		meta: { count },
+	} = await request<HomeQuery>(query);
 
-  return (
-    <main style={{ counterReset: 'work-counter' }}>
-      {renderMetaTags(homepage?._seoMetaTags || [])}
-      <HorizontalScroller>
-        {homepage && (
-          <Tile>
-            <div className="mx-7 py-12 xl:m-0 xl:w-[60vw] xl:max-w-[900px] xl:flex xl:items-center xl:justify-items-center xl:p-32">
-              <div>
-                <div className="uppercase tracking-widest text-sm mb-12 xl:mb-20">
-                  {homepage.title}
-                </div>
-                <h1 className="text-black font-serif mb-12 text-5xl xl:text-8xl tracking-tight">
-                  <UnwrapStructuredText
-                    data={homepage.tagline.value as StructuredTextDocument}
-                  />
-                </h1>
-                <div className="leading-loose prose max-w-none">
-                  <StructuredText
-                    data={homepage.description.value as StructuredTextDocument}
-                  />
-                </div>
-              </div>
-            </div>
-          </Tile>
-        )}
-        {works.map((work) => (
-          <Tile key={work.id}>
-            <Work work={work} total={count} />
-          </Tile>
-        ))}
-      </HorizontalScroller>
-    </main>
-  );
+	return (
+		<main style={{ counterReset: "work-counter" }}>
+			{renderMetaTags(homepage?._seoMetaTags || [])}
+			<HorizontalScroller>
+				{homepage && (
+					<Tile>
+						<div className="mx-7 py-12 xl:m-0 xl:w-[60vw] xl:max-w-[900px] xl:flex xl:items-center xl:justify-items-center xl:p-32">
+							<div>
+								<div className="uppercase tracking-widest text-sm mb-12 xl:mb-20">
+									{homepage.title}
+								</div>
+								<h1 className="text-black font-serif mb-12 text-5xl xl:text-8xl tracking-tight">
+									<UnwrapStructuredText
+										data={homepage.tagline.value as StructuredTextDocument}
+									/>
+								</h1>
+								<div className="leading-loose prose max-w-none">
+									<StructuredText
+										data={homepage.description.value as StructuredTextDocument}
+									/>
+								</div>
+							</div>
+						</div>
+					</Tile>
+				)}
+				{works.map((work) => (
+					<Tile key={work.id}>
+						<Work work={work} total={count} />
+					</Tile>
+				))}
+			</HorizontalScroller>
+		</main>
+	);
 }
