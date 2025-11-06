51a804e7283f469ccaeca697e633371f29aa3652
Stefano Verna <s.verna@datocms.com>
2025-11-06 22:42:09 +0100

    gql.tada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-  ],
+  integrations: [tailwind()],
   output: 'static',
+  trailingSlash: 'never',
+  env: {
+    schema: {
+      DATOCMS_API_TOKEN: envField.string({
+        context: 'server',
+        access: 'secret',
+      }),
+    },
+  },
   vite: {
     ssr: {
       noExternal: ['datocms-structured-text-utils', '@datocms/astro'],
--- src/lib/dato.ts
+++ src/lib/dato.ts
@@ -1,21 +1,17 @@
-export async function request<TResult = unknown, TVariables = Record<string, unknown>>(
-  query: string,
-  variables?: TVariables
-): Promise<TResult> {
-  const response = await fetch("https://graphql.datocms.com/", {
-    headers: {
-      Authorization: `Bearer ${import.meta.env.DATOCMS_API_TOKEN}`,
-      "X-Exclude-Invalid": "true",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({ query, variables }),
+import {
+  executeQueryWithAutoPagination,
+  type ExecuteQueryOptions,
+} from '@datocms/cda-client';
+import { DATOCMS_API_TOKEN } from 'astro:env/server';
+import type { TadaDocumentNode } from 'gql.tada';
+
+export async function request<Result, Variables>(
+  query: TadaDocumentNode<Result, Variables>,
+  options: Pick<ExecuteQueryOptions<Variables>, 'variables'>
+) {
+  return executeQueryWithAutoPagination(query, {
+    ...options,
+    excludeInvalid: true,
+    token: DATOCMS_API_TOKEN,
   });
-
-  if (!response.ok) {
-    throw new Error(`Failed request: ${response.statusText}`);
-  }
-
-  const result = await response.json();
-  return result.data;
 }
